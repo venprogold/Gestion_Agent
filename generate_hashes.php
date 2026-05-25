06faff3917e4ebcf26e85c1688cf827d79d5a92f
14-1Gabriel <tgab31686@gmail.com>
2026-05-25 01:14:11 +0100

    Suppr

--- generate_hashes.php
+++
@@ -1,7 +0,0 @@
-<?php
-// generate_hashes.php
-echo "Hash pour 'responsable123' : " . password_hash('responsable123', PASSWORD_DEFAULT) . "\n";
-echo "Hash pour 'agent123' : " . password_hash('agent123', PASSWORD_DEFAULT) . "\n";
-echo "\n";
-echo "Copie ces hashs dans ton fichier database.sql !\n";
-?>
